6d6c3d03701e4c236f473d29b0f918db30fd55fa
rachaelwasson <rwasson@wisc.edu>
2021-10-30 12:19:31 -0500

    tried to add menu nav functionality to reminders activity, my emulator keeps crashing tho

--- app/src/main/java/com/example/charred/MainActivity.java
+++ app/src/main/java/com/example/charred/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.charred;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    private NavigationBarView bottomNavigationView;
+    Intent remindersIntent = new Intent(this, RemindersActivity.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        //bottomNavigationView.setOnItemSelectedListener(bottomnavFunction);
     }
+
+//    private NavigationBarView.OnItemSelectedListener bottomnavFunction = new NavigationBarView.OnItemSelectedListener() {
+//        @Override
+//        public boolean onNavigationItemSelected(MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.remindersMenuItem:
+//                    startActivity(remindersIntent);
+//                    break;
+//            }
+//            return true;
+//        }
+//    };
+
 }
